beec25ed422b1f216d0be59567dd043022f821b4
jshim0978 <43781129+jshim0978@users.noreply.github.com>
2019-12-15 20:24:59 +0900

    arr and doub

---
+++ bytecode/src/listener/main/BytecodeGenListenerHelper.java
@@ -0,0 +1,182 @@
+package listener.main;
+
+
+import generated.MiniCParser;
+import generated.MiniCParser.ExprContext;
+import generated.MiniCParser.Fun_declContext;
+import generated.MiniCParser.If_stmtContext;
+import generated.MiniCParser.Local_declContext;
+import generated.MiniCParser.ParamContext;
+import generated.MiniCParser.ParamsContext;
+import generated.MiniCParser.Type_specContext;
+import generated.MiniCParser.Var_declContext;
+
+import listener.main.SymbolTable.Type;
+
+public class BytecodeGenListenerHelper {
+	
+	// <boolean functions>
+	
+	static boolean isFunDecl(MiniCParser.ProgramContext ctx, int i) {
+		return ctx.getChild(i).getChild(0) instanceof MiniCParser.Fun_declContext;
+	}
+	
+	// type_spec IDENT '[' ']'
+	static boolean isArrayParamDecl(ParamContext param) {
+		return param.getChildCount() == 4;
+	}
+	
+	// global vars
+	static int initVal(Var_declContext ctx) {
+		return Integer.parseInt(ctx.LITERAL().getText());
+	}
+
+	// var_decl	: type_spec IDENT '=' LITERAL ';
+	static boolean isDeclWithInit(Var_declContext ctx) {
+		return ctx.getChildCount() == 5 ;
+	}
+	// var_decl	: type_spec IDENT '[' LITERAL ']' ';'
+	static boolean isArrayDecl(Var_declContext ctx) {
+		return ctx.getChildCount() == 6;
+	}
+
+	// <local vars>
+	// local_decl	: type_spec IDENT '[' LITERAL ']' ';'
+	static int initVal(Local_declContext ctx) {
+		return Integer.parseInt(ctx.LITERAL().getText());
+	}
+
+	static boolean isArrayDecl(Local_declContext ctx) {
+		return ctx.getChildCount() == 6;
+	}
+	
+	static boolean isDeclWithInit(Local_declContext ctx) {
+		return ctx.getChildCount() == 5 ;
+	}
+	
+	static boolean isVoidF(Fun_declContext ctx) {
+			// <Fill in 0>
+		//get first child of ctx
+		//int add(int a , int b)
+		//-> int 
+		//returns 1 if is 'void'
+		return ctx.getChild(0).getText().equals("void");
+	}
+	
+	static boolean isIntReturn(MiniCParser.Return_stmtContext ctx) {
+		
+		return ctx.getChildCount() ==3;
+	}
+
+
+	static boolean isVoidReturn(MiniCParser.Return_stmtContext ctx) {
+		return ctx.getChildCount() == 2;
+	}
+	
+	// <information extraction>
+	static String getStackSize(Fun_declContext ctx) {
+		return "32";
+	}
+	static String getLocalVarSize(Fun_declContext ctx) {
+		return "32";
+	}
+	static String getTypeText(Type_specContext typespec) {
+			// <Fill in 1>
+		//if type string equals int 
+		String type = typespec.getText(); 
+		if(type.compareTo("int") == 0)
+			return "I";
+		
+		else if(type.compareTo("double") == 0)
+			return "D";
+		
+		//other cases left for else statements 
+		return "";
+	}
+
+	// params
+	static String getParamName(ParamContext param) {
+		// <Fill in 2>
+		//get identifier text
+		return param.IDENT().getText();
+	}
+	
+	static Type getParamType(ParamContext param) {
+		if(param.getChild(0).getText().equals("int"))
+			return Type.INT;
+		
+		else if(param.getChild(0).getText().equals("double"))
+			return Type.DOUBLE;
+		
+		return null;
+	}
+	
+	
+	static String getParamTypesText(ParamsContext params) {
+		String typeText = "";
+		
+		for(int i = 0; i < params.param().size(); i++) {
+			MiniCParser.Type_specContext typespec = (MiniCParser.Type_specContext)  params.param(i).getChild(0);
+			typeText += getTypeText(typespec); // + ";";
+		}
+		return typeText;
+	}
+	
+	
+	static String getLocalVarName(Local_declContext local_decl) {
+		// <Fill in 3>
+		//get identifier text
+		return local_decl.IDENT().getText();
+	}
+	
+	static String getFunName(Fun_declContext ctx) {
+		// <Fill in 4>
+		//get identifier text
+		return ctx.IDENT().getText();
+	}
+	
+	static String getFunName(ExprContext ctx) {
+		// <Fill in 5>
+		//get identifier text
+		return ctx.IDENT().getText();
+	}
+	
+	static boolean noElse(If_stmtContext ctx) {
+		return ctx.getChildCount() < 5;
+	}
+	
+	static String getFunProlog() {
+		// return ".class public Test .....
+		// ...
+		// invokenonvirtual java/lang/Object/<init>()
+		// return
+		// .end method"
+		
+		// <Fill in 6>
+		String prolog = "\n\n";
+		
+
+		prolog += ".class public "
+				
+			+ getCurrentClassName()
+			+ "\n"
+			//.class public Test
+			+ ".super java/lang/Object\n\n"
+			//.super java/lang/Object
+			+".method public <init>()V\n"
+			//.method public <init>()V
+			+ "aload_0\n"
+			//aload_0
+			+ "invokenonvirtual java/lang/Object/<init>()V\n"
+			//invokenonvirtual java/lang/Object/<init>()V
+			+ "return\n"
+			//return
+			+ ".end method\n\n";
+			//.end method
+		return prolog;
+	}
+	
+	static String getCurrentClassName() {
+		return "Test";
+	}
+}
---
+++ bytecode/src/listener/main/SymbolTable.java
@@ -0,0 +1,200 @@
+package listener.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import generated.MiniCParser;
+import generated.MiniCParser.Fun_declContext;
+import generated.MiniCParser.Local_declContext;
+import generated.MiniCParser.Var_declContext;
+import static listener.main.BytecodeGenListenerHelper.*;
+
+
+public class SymbolTable {
+	enum Type {
+		INT, INTARRAY, VOID, ERROR, DOUBLE
+		// string double ,,,
+	}
+	
+	static public class VarInfo {
+		Type type; 
+		int id;
+		int initVal;
+		
+		public VarInfo(Type type,  int id, int initVal) {
+			this.type = type;
+			this.id = id;
+			this.initVal = initVal;
+		}
+		public VarInfo(Type type,  int id) {
+			this.type = type;
+			this.id = id;
+			this.initVal = 0;
+		}
+	}
+	
+	static public class FInfo {
+		public String sigStr;
+	}
+	
+	private Map<String, VarInfo> _lsymtable = new HashMap<>();	// local v.
+	private Map<String, VarInfo> _gsymtable = new HashMap<>();	// global v.
+	private Map<String, FInfo> _fsymtable = new HashMap<>();	// function 
+	
+		
+	private int _globalVarID = 0;
+	private int _localVarID = 0;
+	private int _labelID = 0;
+	private int _tempVarID = 0;
+	
+	SymbolTable(){
+		initFunDecl();
+		initFunTable();
+	}
+	
+	void initFunDecl(){		// at each func decl
+		_localVarID = 0;
+		_labelID = 0;
+		_tempVarID = 32;		
+	}
+	
+	void putLocalVar(String varname, Type type){
+		//<Fill here 0>
+		//hash as (name, type)
+		this._lsymtable.put(varname, new VarInfo(type, _localVarID++));
+	}
+	
+	void putGlobalVar(String varname, Type type){
+		//<Fill here 1>
+		//hash as (name, type)
+		this._gsymtable.put(varname, new VarInfo(type, _globalVarID++));
+	}
+	
+	void putLocalVarWithInitVal(String varname, Type type, int initVar){
+		//<Fill here 2>
+		//hash as (name, type) with init value
+		this._lsymtable.put(varname, new VarInfo(type, _localVarID++, initVar));
+	}
+	void putGlobalVarWithInitVal(String varname, Type type, int initVar){
+		//<Fill here 3>
+		//hash as (name, type) with init value
+		this._gsymtable.put(varname, new VarInfo(type, _globalVarID++, initVar));
+	
+	}
+	
+	void putParams(MiniCParser.ParamsContext params) {
+		for(int i = 0; i < params.param().size(); i++) {
+		//<Fill here 4>
+			//for each parameters
+			String varname = getParamName(params.param(i));
+			Type type = getParamType(params.param(i));
+			//hash as (name, type)
+			this._lsymtable.put(varname, new VarInfo(type, _localVarID++));
+
+		}
+	}
+	
+	private void initFunTable() {
+		FInfo printlninfo = new FInfo();
+		printlninfo.sigStr = "java/io/PrintStream/println(I)V";
+		
+		FInfo maininfo = new FInfo();
+		maininfo.sigStr = "main([Ljava/lang/String;)V";
+		_fsymtable.put("_print", printlninfo);
+		_fsymtable.put("main", maininfo);
+	}
+	
+	public String getFunSpecStr(String fname) {		
+		// <Fill here 5>
+		//add / _print
+		return _fsymtable.get(fname).sigStr;
+	}
+
+	public String getFunSpecStr(Fun_declContext ctx) {
+		// <Fill here 6>	
+		//identity of function spec
+		return getFunSpecStr(ctx.IDENT().getText());
+	}
+	
+	public String putFunSpecStr(Fun_declContext ctx) {
+		String fname = getFunName(ctx);
+		String argtype = "";	
+		String rtype = "";
+		String res = "";
+		
+		// <Fill here 7>	
+		
+		if(ctx.type_spec().getText().equals("void"))
+			rtype += "V";//add()V
+		
+		else if (ctx.type_spec().getText().equals("double"))
+			rtype += "D";
+		
+		else
+			rtype += "I";//add()I
+
+		
+		for(int i = 0; i < ctx.params().param().size(); i++) {
+			if(ctx.params().param(i).type_spec().getText().equals("void"))
+				argtype += "V";//add(V)
+			else if (ctx.params().param(i).type_spec().getText().equals("double"))
+				argtype += "D";
+			else//only void or integers at this time
+				argtype += "I";//add(I)
+		}
+		res =  fname + "(" + argtype + ")" + rtype; 
+		FInfo finfo = new FInfo();//
+		finfo.sigStr = res;
+		_fsymtable.put(fname, finfo);
+		
+		return res;
+	}
+	
+	String getVarId(String name){
+		// <Fill here 8>	
+		if(_lsymtable.containsKey(name)) //hashmap search in locals
+			return Integer.toString(_lsymtable.get(name).id);
+		else if(_gsymtable.containsKey(name)) //hashmap search in globals
+			return Integer.toString(_gsymtable.get(name).id);
+		return "";//return nothing if string not in either table
+	}
+	
+	Type getVarType(String name){
+		VarInfo lvar = (VarInfo) _lsymtable.get(name);
+		if (lvar != null) {
+			return lvar.type;
+		}
+		
+		VarInfo gvar = (VarInfo) _gsymtable.get(name);
+		if (gvar != null) {
+			return gvar.type;
+		}
+		
+		return Type.ERROR;	
+	}
+	String newLabel() {
+		return "label" + _labelID++;
+	}
+	
+	String newTempVar() {
+		String id = "";
+		return id + _tempVarID--;
+	}
+
+	// global
+	public String getVarId(Var_declContext ctx) {
+		// <Fill here 9>
+		//same as locals
+		String sname = "";
+		sname += getVarId(ctx.IDENT().getText());
+		return sname;
+	}
+
+	// local
+	public String getVarId(Local_declContext ctx) {
+		String sname = "";
+		sname += getVarId(ctx.IDENT().getText());
+		return sname;
+	}
+	
+}
